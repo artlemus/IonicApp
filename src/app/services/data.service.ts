a39121d89e9c7201de9768c9b44beac3922a4369
artlemus <artlemus91@gmail.com>
2020-04-05 11:30:02 -0700

    new updates

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { Message } from "../models/message";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DataService {
+  constructor() {}
+
+  allMessage: Message[] = [];
+
+  public saveMessage(message) {
+    this.allMessage.push(message);
+  }
+
+  public getAllMessaged() {
+    return this.allMessage;
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DataService } from "../services/data.service";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
 
-  constructor() {}
+  
+  constructor(private data: DataService) {
+   
+  }
 
+  getMessages() {
+    console.log(this.data.getAllMessaged());
+    return this.data.getAllMessaged();
+
+  }
+  
 }
+
