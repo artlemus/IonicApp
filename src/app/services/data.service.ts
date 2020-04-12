4551b5c94cdeb73b047a1d69c7dc95650be7c58c
artlemus <artlemus91@gmail.com>
2020-04-12 11:51:14 -0700

    class 3 working chat

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,19 +1,57 @@
 import { Injectable } from "@angular/core";
 import { Message } from "../models/message";
+import { map } from  'rxjs/operators'
+import { Observable } from 'rxjs';
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+import { firestore } from 'firebase';
+
 
 @Injectable({
   providedIn: "root",
 })
 export class DataService {
-  constructor() {}
 
-  allMessage: Message[] = [];
+  
+  
 
-  public saveMessage(message) {
-    this.allMessage.push(message);
+  allMessages: Observable<Message[]>;
+
+  //pipeline to firebase database
+  messageCollection: AngularFirestoreCollection<Message>; 
+
+  constructor(private fb: AngularFirestore) {
+    // initialize connection to app firebase
+    this.messageCollection = fb.collection<Message>('posts');
+  }
+// way to read data without date
+  // retrieveMessagesFromDB() {
+  //   this.allMessages = this.messageCollection.valueChanges();
+  // }
+
+  retrieveMessagesFromDB(){
+    this.allMessages = this.messageCollection.snapshotChanges().pipe(
+      map(actions => {
+          return actions.map(a => {
+              let data = a.payload.doc.data();
+              var d: any = data.createdOn; // <- firebase data format
+              if(d){
+                data.createdOn = new firestore.Timestamp(d.seconds, d.nanoseconds).toDate();
+              }
+              return {... data }
+          })
+      })
+    );
   }
 
-  public getAllMessaged() {
-    return this.allMessage;
+
+  public saveMessage(message) {
+
+    var plain = Object.assign({}, message);
+    this.messageCollection.add(plain);
+  }
+
+  public getAllMessages() {
+    this.retrieveMessagesFromDB(); //subscribe to changes
+    return this.allMessages;
   }
 }
